ace618ac23116d1686abe52c9436d7d12977f0a5
killian31 <80256683+killian31@users.noreply.github.com>
2022-06-20 20:11:04 +0200

    create scraper

--- scraper.py
+++ scraper.py
@@ -2,21 +2,54 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import datetime
+import dateutil.relativedelta
 
 load_dotenv()
-bearer_token = os.getenv("BEARER_TOKEN")
 
-def search_twitter(query, tweet_fields, bearer_token = ""):
+def auth():
+    return os.getenv('BEARER_TOKEN')
+
+def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
 
-    url = "https://api.twitter.com/1.1/search/tweet.json?q={}&result_type={}".format(
-        query, tweet_fields
-    )
-    print(url)
-    response = requests.get(url)
-    print(response.status_code)
+def create_url(keyword, start_date, end_date, max_results = 10):
+    
+    search_url = "https://api.twitter.com/2/tweets/search/recent"
 
+    query_params = {'query': keyword,
+                    'start_time': start_date + "T00:00:00.000Z",
+                    'end_time': end_date + "T00:00:00.000Z",
+                    'max_results': max_results,
+                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+                    'next_token': {}}
+    return (search_url, query_params)
+
+def connect_to_endpoint(url, headers, params, next_token = None):
+    params['next_token'] = next_token   
+    response = requests.get(url, headers = headers, params = params)
+    print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
-    return response.content()
+    return response.json()
 
+def scrap(topic, start_date = str(datetime.datetime.now() + dateutil.relativedelta.relativedelta(days=-6))[:10], end_date = str(datetime.datetime.now())[:10], max_results=15):
+    bearer_token = auth()
+    headers = create_headers(bearer_token)
+    keyword = f"{topic} lang:fr"
+    max_results = max_results
+
+    url = create_url(keyword, start_date, end_date, max_results)
+    json_response = connect_to_endpoint(url[0], headers, url[1])
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+def create_data(topic, start_date = str(datetime.datetime.now() + dateutil.relativedelta.relativedelta(days=-6))[:10], end_date = str(datetime.datetime.now())[:10], max_results=15):
+    json_file = scrap(topic, start_date, end_date, max_results)
+
+if __name__ == "__main__":
+    pass
+    # scrap("piscine")
